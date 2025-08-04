662a48b44f5a3777db67d1c0bfcc76287fdf12fc
kamal930780 <kamalchandiramani74@gmail.com>
2025-08-04 07:08:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const emojis =["😊","😊","❤️","❤️","😘","😘","💕","💕","👩‍❤️‍👨","👩‍❤️‍👨","😁","😁","🌕","🌕","👍","👍"];
+        var shuf_emojis =emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+
+        for (var i=0; i<emojis.length; i++) {
+            let box=document.createElement('div');
+            box.className='item';
+            box.innerHTML=shuf_emojis[i]
+
+            box.onclick=function(){
+                this.classList.add('boxopen');
+                setTimeout(function(){
+                    if(document.querySelectorAll('.boxopen').length > 1){
+                        if(document.querySelectorAll('.boxopen')[0].innerHTML ==
+                         document.querySelectorAll('.boxopen')[1].innerHTML){
+                            document.querySelectorAll('.boxopen')[0].classList.add('boxmatch');
+                            document.querySelectorAll('.boxopen')[1].classList.add('boxmatch');
+                            document.querySelectorAll('.boxopen')[1].classList.remove('boxopen');
+                            document.querySelectorAll('.boxopen')[0].classList.remove('boxopen');
+                    
+                            if(document.querySelectorAll('.boxmatch').length ==emojis.length){
+                                alert("CONGRATULATIONS BUDDY!! you have completed all pairs")
+                            }
+
+                            }else {
+                                document.querySelectorAll('.boxopen')[1].classList.remove('boxopen');
+                                document.querySelectorAll('.boxopen')[0].classList.remove('boxopen');
+                            }
+                    }
+                 },500)
+                }
+            document.querySelector('.game').appendChild(box);
+        
+
+
+        }
